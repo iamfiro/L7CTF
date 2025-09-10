cf92cb7951a21bcf15bd9079812c463f8299d7ef
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-10 20:31:46 +0900

    Refactor Hero and Sponsor components for improved clarity and consistency; update class names for better semantics, enhance typography with descriptive labels, and adjust spacing for a more cohesive layout.

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Typo } from "@/components/ui";
 import Spacing from "@/components/ui/spacing";
-import { HStack, VStack } from "@/components/ui/stack";
+import { FlexAlign, HStack, VStack } from "@/components/ui/stack";
 
 import s from "./style.module.scss";
 
@@ -11,11 +11,11 @@ export default function Hero() {
       <div className={s.light_center} />
       <div className={s.light_right} />
       <div className={s.hero_text}>
-        <VStack gap={12}>
+        <VStack gap={12} align={FlexAlign.Center}>
           <h2 className={s.hero_subtitle}>We hack the</h2>
           <h1 className={s.hero_title}>2025 Layer7 CTF</h1>
         </VStack>
-        <Typo.BodyLarge className={s.hero_text_date}>
+        <Typo.BodyLarge className={s.hero_description}>
           2025.11.24 - 선린인터넷고등학교
         </Typo.BodyLarge>
         <HStack gap={14} className={s.buttons}>
@@ -25,7 +25,7 @@ export default function Hero() {
           <Button size="lg">Layer7 CTF 참가 신청하기</Button>
         </HStack>
       </div>
-      <Spacing size={150} />
+      <Spacing size={120} />
       <img src="/images/hero/map.svg" alt="Layer7 CTF" className={s.hero_map} />
       <div className={s.hero_map_overlay} />
     </section>
--- src/components/home/sponsor/index.tsx
+++ src/components/home/sponsor/index.tsx
@@ -6,22 +6,22 @@ import s from "./style.module.scss";
 export default function Sponsor() {
   return (
     <section className={s.sponsor}>
-      <div className={s.sponsor_wrapper}>
+      <div className={s.sponsor_wrap}>
         <Typo.Body className={s.sponsor_text}>
           Layer7 CTF는 다음과 같은 기업들과 함께합니다
         </Typo.Body>
-        <img src="/images/sponsor/theori.png" alt="sponsor" />
-        <img src="/images/sponsor/hspace.png" alt="sponsor" />
+        <img src="/images/sponsor/theori.png" alt="Theori sponsor" />
+        <img src="/images/sponsor/hspace.png" alt="Hspace sponsor" />
       </div>
-      <div className={s.sponsor_wrapper}>
+      <div className={s.sponsor_wrap}>
         <Typo.Body className={s.sponsor_text}>대회 주최</Typo.Body>
         <HStack gap={24}>
           <img
             src="/images/sponsor/sunrin.png"
-            alt="sponsor"
+            alt="Sunrin sponsor"
             className={s.sunrin}
           />
-          <img src="/layer7.svg" alt="sponsor" className={s.layer7} />
+          <img src="/layer7.svg" alt="Layer7 sponsor" className={s.layer7} />
         </HStack>
       </div>
     </section>
