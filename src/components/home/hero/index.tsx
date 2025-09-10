74ee896e5fab2f13f14e1b4d61b7bdf25335d8b0
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-10 10:41:35 +0900

    Refactor Hero component to include new button elements and adjust typography; enhance styling with additional light background elements and spacing improvements.

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,19 +1,28 @@
-import { Typo } from "@/components/ui";
+import { Button, Typo } from "@/components/ui";
 import Spacing from "@/components/ui/spacing";
+import { HStack } from "@/components/ui/stack";
 
 import s from "./style.module.scss";
 
 export default function Hero() {
   return (
     <section className={s.hero}>
+      <div className={s.hero_light} />
+      <div className={s.hero_light_sub} />
       <div className={s.hero_text}>
         <div className={s.hero_text_title}>
           <h2>We hack the</h2>
           <h1>2025 Layer7 CTF</h1>
         </div>
-        <Typo.Headline className={s.hero_text_date}>
+        <Typo.BodyLarge className={s.hero_text_date}>
           2025.11.24 - 선린인터넷고등학교
-        </Typo.Headline>
+        </Typo.BodyLarge>
+        <HStack gap={14} className={s.buttons}>
+          <Button size="lg" variant="secondary">
+            문의하기
+          </Button>
+          <Button size="lg">대회 참가 신청하기</Button>
+        </HStack>
       </div>
       <Spacing size={150} />
       <img src="/images/hero/map.svg" alt="Layer7 CTF" className={s.hero_map} />
