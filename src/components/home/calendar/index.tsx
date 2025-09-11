75bb75832e03c45ac568a934cdd71acfa71c78f0
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-11 13:43:21 +0900

    Refactor Calendar and Money components for improved layout and styling; implement HStack for better alignment, enhance card information display, and introduce a carousel for showcasing prizes. Update styles for consistency and responsiveness.

--- src/components/home/calendar/index.tsx
+++ src/components/home/calendar/index.tsx
@@ -1,7 +1,7 @@
 import { File, Flag, Trophy } from "lucide-react";
 
 import { Typo } from "@/components/ui";
-import { HStack, VStack } from "@/components/ui/stack";
+import { FlexAlign, FlexJustify, HStack, VStack } from "@/components/ui/stack";
 
 import s from "./style.module.scss";
 
@@ -19,35 +19,75 @@ export default function Calendar() {
         </VStack>
         <VStack style={{ width: "50%" }} gap={12}>
           <article className={`${s.card} ${s.card_now}`}>
-            <VStack gap={8}>
-              <Typo.Display className={s.card_title}>접수 기간</Typo.Display>
-              <Typo.Body className={s.card_date}>
-                2025년 11월 24일 ~ 2025년 12월 10일
-              </Typo.Body>
-            </VStack>
-            <File />
+            <HStack
+              justify={FlexJustify.Between}
+              align={FlexAlign.Center}
+              fullWidth
+            >
+              <VStack gap={8}>
+                <Typo.Display className={s.card_title}>접수 기간</Typo.Display>
+                <Typo.Body className={s.card_date}>
+                  2025년 11월 24일 ~ 2025년 12월 10일
+                </Typo.Body>
+              </VStack>
+              <File />
+            </HStack>
           </article>
           <article className={`${s.card}`}>
-            <VStack gap={8}>
-              <Typo.Display className={s.card_title}>
-                CTF 예선 대회
-              </Typo.Display>
-              <Typo.Body className={s.card_date}>
-                2025년 11월 24일 ~ 2025년 12월 10일
+            <HStack
+              justify={FlexJustify.Between}
+              align={FlexAlign.Center}
+              fullWidth
+            >
+              <VStack gap={8}>
+                <Typo.Display className={s.card_title}>
+                  CTF 예선 대회
+                </Typo.Display>
+                <Typo.Body className={s.card_date}>
+                  2025년 11월 24일 ~ 2025년 12월 10일
+                </Typo.Body>
+              </VStack>
+              <Flag />
+            </HStack>
+            <HStack gap={14}>
+              <Typo.Body className={s.card_info_name}>본선 진출</Typo.Body>
+              <Typo.Body className={s.card_info_value}>상위 20인</Typo.Body>
+            </HStack>
+            <HStack gap={14}>
+              <Typo.Body className={s.card_info_name}>참가 장소</Typo.Body>
+              <Typo.Body className={s.card_info_value}>
+                선린인터넷고등학교
               </Typo.Body>
-            </VStack>
-            <Flag />
+            </HStack>
           </article>
           <article className={`${s.card}`}>
-            <VStack gap={8}>
-              <Typo.Display className={s.card_title}>
-                CTF 본선 대회
-              </Typo.Display>
-              <Typo.Body className={s.card_date}>
-                2025년 11월 24일 ~ 2025년 12월 10일
+            <HStack
+              justify={FlexJustify.Between}
+              align={FlexAlign.Center}
+              fullWidth
+            >
+              <VStack gap={8}>
+                <Typo.Display className={s.card_title}>
+                  CTF 본선 대회
+                </Typo.Display>
+                <Typo.Body className={s.card_date}>
+                  2025년 11월 24일 ~ 2025년 12월 10일
+                </Typo.Body>
+              </VStack>
+              <Trophy />
+            </HStack>
+            <HStack gap={14}>
+              <Typo.Body className={s.card_info_name}>참가 대상</Typo.Body>
+              <Typo.Body className={s.card_info_value}>
+                예선 상위 20인
               </Typo.Body>
-            </VStack>
-            <Trophy />
+            </HStack>
+            <HStack gap={14}>
+              <Typo.Body className={s.card_info_name}>참가 장소</Typo.Body>
+              <Typo.Body className={s.card_info_value}>
+                선린인터넷고등학교
+              </Typo.Body>
+            </HStack>
           </article>
         </VStack>
       </HStack>
--- src/components/home/money/index.tsx
+++ src/components/home/money/index.tsx
@@ -1,12 +1,65 @@
 import { Typo } from "@/components/ui";
+import { FlexAlign, VStack } from "@/components/ui/stack";
 
 import s from "./style.module.scss";
 
 export default function Money() {
+  const goods = [
+    { id: 1, src: "/images/goods/airpods.png", alt: "상품 [AirPods]" },
+    { id: 2, src: "/images/goods/iphone.webp", alt: "상품 [AirPods]" },
+    { id: 3, src: "/images/goods/dreamhack.webp", alt: "상품 [AirPods]" },
+  ];
+
+  // 무한 루프를 위해 상품 배열을 2번 복사
+  const duplicatedGoods = [...goods, ...goods];
+
   return (
     <section className={s.money}>
-      <Typo.Headline>총 상금</Typo.Headline>
-      <Typo.Headline className={s.money_title}>3,000,000원</Typo.Headline>
+      <VStack align={FlexAlign.Center}>
+        <Typo.Headline>대회 상금</Typo.Headline>
+        <Typo.Headline className={s.money_title}>3,000,000원</Typo.Headline>
+
+        <table className={s.table}>
+          <thead>
+            <tr>
+              <th>구분</th>
+              <th>주관</th>
+              <th>인원</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>본상</td>
+              <td>선린인터넷고등학교장</td>
+              <td>중고등부/고등부 상위 3명씩</td>
+            </tr>
+            <tr>
+              <td>티오리 대표상</td>
+              <td>Theori</td>
+              <td>1명</td>
+            </tr>
+            <tr>
+              <td>Hspace 대표상</td>
+              <td>Hspace</td>
+              <td>1명</td>
+            </tr>
+          </tbody>
+        </table>
+      </VStack>
+      <VStack align={FlexAlign.Center} gap={48}>
+        <Typo.BodyLarge>이 외의 다양한 상품이 준비되어 있어요</Typo.BodyLarge>
+        <div className={s.carousel_container}>
+          <div className={s.carousel_track}>
+            <div className={s.carousel_slides}>
+              {duplicatedGoods.map((item, index) => (
+                <div key={`${item.id}-${index}`} className={s.carousel_slide}>
+                  <img src={item.src} alt={item.alt} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </VStack>
     </section>
   );
 }
