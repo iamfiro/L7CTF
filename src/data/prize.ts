fd528e1c3aafe38bd209d39e95b54e7f85fb54b3
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-09-28 20:46:38 +0900

    Refactor CompetitionAbout and FormAbout components for improved layout and styling; enhance responsiveness and visual consistency across the home layout. Update button styles and typography for better alignment.

---
+++ src/data/prize.ts
@@ -0,0 +1,31 @@
+export const Prize = {
+  total: 3000000,
+  middle: {
+    first: {
+      medal: true,
+      prize: 250000,
+    },
+    second: {
+      medal: true,
+      prize: 250000,
+    },
+    third: {
+      medal: true,
+      prize: 100000,
+    },
+  },
+  general: {
+    first: {
+      medal: false,
+      prize: 500000,
+    },
+    second: {
+      medal: false,
+      prize: 300000,
+    },
+    third: {
+      medal: false,
+      prize: 200000,
+    },
+  },
+};
