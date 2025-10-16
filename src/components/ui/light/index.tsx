3f41c2bba3d75b8dd629893c6d62993c6e7a558a
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-10-17 00:26:43 +0900

    Update Light component to wrap elements in a div with reduced opacity for visual effect; adjust SCSS to fix closing bracket alignment in light.module.scss.

--- src/components/ui/light/index.tsx
+++ src/components/ui/light/index.tsx
@@ -2,10 +2,10 @@ import s from "./light.module.scss";
 
 export default function Light() {
   return (
-    <>
+    <div style={{ opacity: 0.7 }}>
       <div className={s.light} />
       <div className={s.light_center} />
       <div className={s.light_right} />
-    </>
+    </div>
   );
 }
